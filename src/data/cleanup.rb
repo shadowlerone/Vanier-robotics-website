a5eb5e49b6f92f690874138db355a24211bf712c
shadowlerone <andrae.lewis@gmail.com>
2021-02-21 11:33:17 -0500

    Added roster images

---
+++ src/data/cleanup.rb
@@ -0,0 +1,9 @@
+require 'json'
+
+data = JSON.parse File.open('members.json').read
+data['members'].each {|m| m.delete_if {|key, value| true if key.downcase.include? "feedback" or key.downcase.include? "point" or key == "ID" or key == "Start time" or key == "Completion time" or key == "Email"}}
+
+count = {}
+data['members'].each {|m| count[m["Name"]] == nil ? count[m["Name"]] = 1 : count[m["Name"]] += 1}
+puts count.filter {|_,i| i > 1}
+File.open("members2.json", "w").write JSON.generate data
